4c2d29e24a4c0e021b566dec88db5e0a59b2126c
Victor Presumido <victor.presumido@gmail.com>
2017-12-15 22:34:58 -0200

    Refactor DetailView into more readable:
    `CompanyRetrieveUpdateDestroyView`
    Also implement `company_service.get_company` method

--- gadgets/api/urls.py
+++ gadgets/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CompanyListCreateView, DetailsView#, CompanyList
+from .views import CompanyListCreateView, CompanyRetrieveUpdateDestroyView#, CompanyList
 
 urlpatterns =[
     path('companies/', CompanyListCreateView.as_view(), name="create"),
-    path('companies/<int:pk>', DetailsView.as_view(), name="details"),
+    path('companies/<int:pk>', CompanyRetrieveUpdateDestroyView.as_view(), name="details"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- gadgets/api/views.py
+++ gadgets/api/views.py
@@ -1,31 +1,16 @@
-from rest_framework.filters import (
-        SearchFilter,
-        OrderingFilter,
-    )
-
-from rest_framework.generics import (
-    RetrieveUpdateDestroyAPIView
-    )
-
-from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
 from .serializers import CompanySerializer
 from .models import Company
-
 from . import company_service
 
 class CompanyListCreateView(APIView):
     """This class defines the create behavior of our rest api."""
 
     def get(self, request, format=None):
-        """
-        Optionally restricts the returned companys to a given name,
-        by filtering against a `name` query parameter in the URL.
-        """
-
+        """Retrieves the list of companies. """
         companies = company_service.get_companies(request.query_params)
         serializer = CompanySerializer(companies, many=True)
         return Response(serializer.data)
@@ -40,8 +25,27 @@ class CompanyListCreateView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class DetailsView(RetrieveUpdateDestroyAPIView):
+class CompanyRetrieveUpdateDestroyView(APIView):
     """This class handles the http GET, PUT and DELETE requests."""
 
-    queryset = Company.objects.all()
-    serializer_class = CompanySerializer
+    def get(self, request, pk, format=None):
+        """Retrieves a single company."""
+        company = company_service.get_company(pk)
+        serializer = CompanySerializer(company)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        """Updates a single company record."""
+        company = company_service.get_company(pk)
+        serializer = CompanySerializer(company, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        """Deletea single company record."""
+        company = company_service.get_company(pk)
+        company.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
