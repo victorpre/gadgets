5351aea0e74afe14a8897d3e49835d75863541eb
Victor Presumido <victor.presumido@gmail.com>
2017-12-15 22:08:06 -0200

    Refactor CompanyListCreateView with get/post
    default actions

--- gadgets/api/views.py
+++ gadgets/api/views.py
@@ -4,9 +4,9 @@ from rest_framework.filters import (
     )
 
 from rest_framework.generics import (
-    ListCreateAPIView,
     RetrieveUpdateDestroyAPIView
     )
+
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -17,37 +17,28 @@ from .models import Company
 
 from . import company_service
 
-#   class CompanyList(APIView):
-#      """
-#      List all companies, or create a new company.
-#      """
-#      def get(self, request, format=None):
-#          companies = Company.objects.all()
-#          serializer = CompanySerializer(companies, many=True)
-#          return Response(serializer.data)
-#
-#      def post(self, request, format=None):
-#          serializer = CompanySerializer(data=request.data)
-#          if serializer.is_valid():
-#              serializer.save()
-#              return Response(serializer.data, status=status.HTTP_201_CREATED)
-#          return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class CompanyListCreateView(ListCreateAPIView):
+class CompanyListCreateView(APIView):
     """This class defines the create behavior of our rest api."""
-    serializer_class = CompanySerializer
 
-    def get_queryset(self):
+    def get(self, request, format=None):
         """
         Optionally restricts the returned companys to a given name,
         by filtering against a `name` query parameter in the URL.
         """
-        return company_service.get_companies(self.request.query_params)
 
-    def perform_create(self, serializer):
+        companies = company_service.get_companies(request.query_params)
+        serializer = CompanySerializer(companies, many=True)
+        return Response(serializer.data)
+
+
+    def post(self, request, format=None):
         """Save the post data when creating a new company."""
-        serializer.save()
+        serializer = CompanySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class DetailsView(RetrieveUpdateDestroyAPIView):
     """This class handles the http GET, PUT and DELETE requests."""
