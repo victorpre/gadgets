e61819b7240d3b954a01d9f7913912142377fe34
Victor Presumido <victor.presumido@gmail.com>
2018-01-16 01:06:28 -0200

    WIP - Split edit/create logic

--- frontend/app/public/Dialog/DialogService.js
+++ frontend/app/public/Dialog/DialogService.js
@@ -5,29 +5,47 @@ function DialogService($mdDialog, DevicesService) {
     openAddDeviceDialog: openAddDeviceDialog,
     cancel: cancel,
     addDevice: addDevice,
-    getDeviceModelOptions: getDeviceModelOptions
+    getDeviceModelOptions: getDeviceModelOptions,
+    editableDevice: {},
+    isCreate: false
   };
+
 
   return service;
 
-  function openAddDeviceDialog(ev){
+  function openAddDeviceDialog(device){
+    if (typeof device !== "undefined") {
+      service.editableDevice = device;
+      service.isCreate = false;
+    } else {
+      service.isCreate = true;
+    }
+
     $mdDialog.show({
       controller: DialogController,
       templateUrl: 'public/Devices/_add_dialog.html',
       parent: angular.element(document.body),
-      targetEvent: ev,
       clickOutsideToClose: true,
+      // refactor
+      onRemoving: function(el, rm){
+        service.editableDevice = {};
+      },
       fullscreen: true
     });
   }
 
   function cancel(){
     $mdDialog.cancel();
+    service.editableDevice = {};
   }
 
   function addDevice(device) {
-    DevicesService.addDevice(device)
-    $mdDialog.hide();
+    if (service.isCreate) {
+      DevicesService.addDevice(device);
+    } else{
+      console.log("TODO: send to backend");
+    }
+    cancel();
   }
 
   function getDeviceModelOptions() {
