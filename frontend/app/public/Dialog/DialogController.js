a792d83dbaaeb5ce0dee50553de84dd57dddda65
Victor Presumido <victor.presumido@gmail.com>
2018-01-16 01:05:17 -0200

    Pass editable device from controller to view
    When initializing DialogController, the device is passed to populate the
    form input fields

--- frontend/app/public/Dialog/DialogController.js
+++ frontend/app/public/Dialog/DialogController.js
@@ -2,4 +2,5 @@ dialog.controller('DialogController', DialogController );
 
 function DialogController($scope, DialogService) {
   $scope.dialogService = DialogService;
+  $scope.device = $scope.dialogService.editableDevice;
 }
