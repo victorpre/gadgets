360b0c0872f7ce499b3759d78cd240d391648013
Victor Presumido <victor.presumido@gmail.com>
2018-01-16 23:58:13 -0200

    Configure middleware to render static files
    Configure NodeJS express middle to serve static files

--- frontend/app/server.js
+++ frontend/app/server.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var server = express();
+
 server.use(express.static(__dirname));
+server.use('/public', express.static(__dirname + '/public'));
+server.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+server.all('/*', function(req, res, next) {
+  res.sendFile('index.html', { root: __dirname });
+});
 
 var port = process.env.PORT || 8081;
 server.listen(port);
