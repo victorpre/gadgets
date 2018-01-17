ab95a37969c6a236e1633073e59f5eef0fe1dee2
Victor Presumido <victor.presumido@gmail.com>
2018-01-16 22:23:03 -0200

    Edit capability to Device
    A device now can be updated and seen through the routes:
    
    - `GET /api/devices/<pk>`
    - `PUT /api/devices/<pk>`

--- backend/api/tests/test_views.py
+++ backend/api/tests/test_views.py
@@ -116,6 +116,26 @@ class DeviceViewTestCase(TestCase):
         """Test the api has device creation capability."""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
 
+    def test_api_can_read_a_device(self):
+        """Test the api has device read capability."""
+        device = Device.objects.get()
+        response = self.client.get(
+            reverse('device_details',
+                    kwargs={'pk': device.id}), format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_api_can_update_device(self):
+        """Test the api can update a given device."""
+        device = Device.objects.get()
+        change_device = self.device_data
+        change_device['capacity'] = 64
+        res = self.client.put(
+            reverse('device_details', kwargs={'pk': device.id}),
+            change_device, format='json'
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
     def test_api_can_delete_a_device(self):
         """Test the api can delete a device."""
         device = Device.objects.get()
