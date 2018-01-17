4f5c73b5a536256f34460273bf39cbad824f83ae
Victor Presumido <victor.presumido@gmail.com>
2018-01-17 00:03:27 -0200

    [WIP] Create Controller and template for companies
    Starting implementing Company list view

---
+++ frontend/app/public/Companies/CompaniesController.js
@@ -0,0 +1,6 @@
+companies.controller('CompaniesController', CompaniesController );
+
+function CompaniesController($scope) {
+
+  $scope.hello = "oie"
+}
--- frontend/app/public/appRoutes.js
+++ frontend/app/public/appRoutes.js
@@ -1,12 +1,18 @@
 angular
     .module('appRoutes', ["ui.router"])
-    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-    $stateProvider.state({
-        name: 'devices',
-        url: '/',
-        templateUrl: 'public/Devices/_devices.html',
-        controller: 'DevicesController'
-    });
+    .config(['$stateProvider','$locationProvider', '$urlRouterProvider', function($stateProvider, $locationProvider, $urlRouterProvider) {
+
+      $stateProvider
+        .state('devices',{
+          url: '/',
+          templateUrl: 'public/Devices/_devices.html',
+          controller: 'DevicesController'
+        })
+        .state('companies',{
+          url: '/companies',
+          templateUrl: 'public/Companies/_companies.html',
+          controller: 'CompaniesController'
+        });
 
     $urlRouterProvider.otherwise('/');
 }]);
