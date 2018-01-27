7abb6c1b8a00a0f2025f074c39fff165a450966a
Victor Presumido <victor.presumido@gmail.com>
2018-01-27 17:51:55 -0200

    Add querying to Device backend

---
+++ backend/api/device_service.py
@@ -0,0 +1,33 @@
+from .models import Device
+from django.http import Http404
+
+def get_devices(filters={}):
+    """
+    Optionally restricts the returned companys to a given name,
+    by filtering against a `name` query parameter in the URL.
+    """
+    queryset = Device.objects.all()
+
+    device_model_id = filters.get('device_model_id', None)
+    capacity = filters.get('capacity', None)
+    color = filters.get('color', None)
+    os_version = filters.get('os_version', None)
+    if device_model_id is not None:
+        queryset = queryset.filter(device_model_id=device_model_id)
+    if capacity is not None:
+        queryset = queryset.filter(capacity=capacity)
+    if color is not None:
+        queryset = queryset.filter(color=color)
+    if os_version is not None:
+        queryset = queryset.filter(os_version=os_version)
+    return queryset
+
+def get_device(pk):
+    """
+    Finds a device by its private key (id).
+    """
+    try:
+        return Device.objects.get(pk=pk)
+    except Device.DoesNotExist:
+        raise Http404
+
